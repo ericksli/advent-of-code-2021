adebac11ada9f90d7d598af32c79e4ed802e76c1
Eric Li <eric@swiftzer.net>
2021-12-01 22:34:28 +0800

    Day 1: Sonar Sweep

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .map { it.toInt() }
+        .fold(input.first().toInt() to 0) { (previousValue, count), currentValue ->
+            currentValue to if (currentValue > previousValue) count + 1 else count
+        }
+        .second
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int = input
+        .map { it.toInt() }
+        .fold(Triple<List<Int>, Int?, Int>(emptyList(), null, 0)) { (lastThree, previousSum, count), currentValue ->
+            val newLastThree = if (lastThree.size <= 3) {
+                lastThree + currentValue
+            } else {
+                lastThree.drop(1) + currentValue
+            }
+            if (newLastThree.size < 3) {
+                Triple(newLastThree, null, 0)
+            } else {
+                val newSum = newLastThree.reduce { acc, i -> acc + i }
+                val newCount = if (previousSum == null) {
+                    0
+                } else if (newSum > previousSum) {
+                    count + 1
+                } else {
+                    count
+                }
+                Triple(newLastThree, newSum, newCount)
+            }
+        }
+        .third
 
     val input = readInput("Day01")
     println(part1(input))
